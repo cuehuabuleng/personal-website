83d350595fb3f10388a0b3b6fe570ca0e49cead7
cuehuabuleng <1079713849@qq.com>
2020-12-04 21:46:10 +0800

    feat:自定义vuepress主题配置，初始化

---
+++ blog/.vuepress/components/articlelist.vue
@@ -0,0 +1,6 @@
+<template>
+<div id="articlelist">
+    <Content />
+
+</div>
+</template>
---
+++ blog/.vuepress/theme/Layout.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        首页
+         <a href="/pages/articles/">文章列表</a>
+        <a href="/pages/about/">关于</a>
+    </div>
+</template>
